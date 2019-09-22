6edae4667a1979bdc6245e371ae6c423fa5d5a0a
Morris Schultz <mfschultz3@gmail.com>
2019-09-22 00:14:12 -0400

    First Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BardicInsultComponent } from './bardic-insult/bardic-insult.component';
+import { BardicInsultComponent } from './components/bardic-insult.component';
+import { BardicInsultService } from './services/bardic-insult.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,9 +14,10 @@ import { BardicInsultComponent } from './bardic-insult/bardic-insult.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [BardicInsultService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/components/bardic-insult.component.ts
+++ src/app/components/bardic-insult.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { BardicInsultService, Insult} from '../services/bardic-insult.service';
+import { } from '@angular/common/';
 
 @Component({
   selector: 'app-bardic-insult',
-  templateUrl: './bardic-insult.component.html',
-  styleUrls: ['./bardic-insult.component.css']
+  templateUrl: '../templates/bardic-insult.component.html',
+  styleUrls: ['../templates/bardic-insult.component.css']
 })
 export class BardicInsultComponent implements OnInit {
 
-  constructor() { }
+  insult = {
+    id: 0,
+    insult: ""
+  };
+  insults;
+
+  constructor(private _bardicInsultService: BardicInsultService) { }
 
   ngOnInit() {
+  }
+
+  getInsult(){
+    this.insults = this._bardicInsultService.getInsults();
+    let ran = Math.ceil(Math.random() * 273);
+    console.log("This is a random num: " + ran);
+    this.insults.forEach(ins => {
+      if (ran == ins.id){
+        this.insult = {
+          id: ins.id,
+          insult: ins.insult
+        }
+        console.log(ins.insult);
+      }
+    }); 
+  }
+
+  resetInsult(){
+    this.insult = {id:0, insult: ""};
   }
 
 }
